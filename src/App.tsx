3b78a211301ec29ed9743a298629d37abef6c030
Borodi21 <Borod_yi21@student.itstep.org>
2024-07-09 15:38:08 +0300

    add test styled component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Card, Container, createTheme } from '@mui/material';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+background:red;`
 
 
+function fetchRegistrationPlate()
+{
+  let api_key = '6adb8dac3bcd1e82b8f138233342b2b1'
+//    let url = `https://baza-gai.com.ua/nomer/${registration_plate}`;
+let url = `https://baza-gai.com.ua/nomer/AE4000IT`;
+
+    fetch(url, {
+        headers: {
+            "Accept": "application/json", "X-Api-Key": api_key 
+        
+        }
+    })
+        .then(r => r.json())
+        .then((data) => {
+            console.log(data)
+             if (data.error) {
+         //   displayError(data.error);
+         console.log(data.error)
+        } else {
+console.log(data)
+         //    displayCarInfo(data)
+        }
+    })
+    .catch((error) => {
+               console.error("Error fetching data: ", error);
+       // displayError("Ошибка при получении данных.");
+            });
+}
+let api_key = '6adb8dac3bcd1e82b8f138233342b2b1'
+let url = `https://baza-gai.com.ua/make/lamborghini/huracan`;
 
 function App() {
   
   return (
+    <>
+    <Button>click me</Button>
+    </>
+    
     /*
     <div className="App">
       <header className="App-header">
@@ -26,15 +64,50 @@ function App() {
       </header>
     </div>
     */
+
+
+
+//function fetchRegistrationPlate(registration_plate)
+
+
+
+
+/*
    <>
  <Container >
        <Card children={'dddddd'}/>
        
  </Container>
 
-   </>
- 
-  );
+   </>*/
+
+//    let url = `https://baza-gai.com.ua/nomer/${registration_plate}`;
+/*
+    fetch(url, {
+        headers: {
+            "Accept": "application/json", "X-Api-Key": api_key 
+        
+        }
+    })
+        .then(r => r.json())
+        .then((data) => {
+            console.log(data)
+             if (data.error) {
+         //   displayError(data.error);
+         console.log(data.error)
+        } else {
+console.log(data)
+         //    displayCarInfo(data)
+        }
+    })
+    .catch((error) => {
+               console.error("Error fetching data: ", error);
+       // displayError("Ошибка при получении данных.");
+            })
+
+            */
+          )
+        
 }
 
 export default App;
