cab401e0939cdb71cb43470a1fe149bcd628eeaa
Borodi21 <Borod_yi21@student.itstep.org>
2024-07-09 18:37:23 +0300

    add carousel

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,158 @@
+import { Card, CardMedia } from "@mui/material";
+import Slider from "react-slick";
+import  "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Carousel.css';
+import img from './pexels-pixabay-210019.jpg'
+
+import { styled, alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('sm')]: {
+        width: '50ch',
+        '&:focus': {
+          width: '40ch',
+        },
+      },
+    },
+  }));
+  
+
+
+const Carousel = () => {
+    const settings = {
+        //dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+       
+
+
+    
+    };
+    return(
+        <div className="image-slider-container">
+        
+      
+        <Slider {...settings}>
+       
+          <Card>
+            <CardMedia
+            component='img'
+            height='300'
+            image={img}
+            />
+      
+            <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Введите марку или модель авто"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+           
+           </Card>
+        
+           <Card>
+            <CardMedia
+            component='img'
+            height='300'
+            image={img}
+            />
+      
+            <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Введите номерной знак авто"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+           
+           </Card>
+       
+           <Card>
+            <CardMedia
+            component='img'
+            height='300'
+            image={img}
+            />
+      
+            <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Введите VIN авто"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+           
+           </Card>
+           <Card>
+            <CardMedia
+            component='img'
+            height='300'
+            image={img}
+            />
+      
+            <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Выберите регион для поиска авто"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+           
+           </Card>
+        
+      </Slider>
+      </div>
+    )
+}
+
+export default Carousel
